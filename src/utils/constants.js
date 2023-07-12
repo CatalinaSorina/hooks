4d6603992472ce669ea207aae90d8470734f101d
Catalina Sorina Gherzan <catalina.sorina@catalina.local>
2023-07-13 00:48:17 +0300

    added colors

---
+++ src/utils/constants.js
@@ -0,0 +1,19 @@
+export const FRIENDS = [
+  { id: "friend-123", name: "John Doe", time: 5000 },
+  { id: "friend-456", name: "Jane Smith", time: 18000 },
+  { id: "friend-789", name: "Alice Johnson", time: 10000 },
+  { id: "friend-012", name: "Bob Anderson", time: 7000 },
+];
+
+export const COLORS = {
+  RED: "#990000",
+  LIGHT_RED: "#FDBCB4",
+  YELLOW: "#FFFFE0",
+  GREEN: "#1B4D3E",
+  LIGHT_GREEN: "#ACE1AF",
+  BLUE: "#468FEA",
+  LIGHT_BLUE: "#AFDBF5",
+  GRAY: "#242124",
+  LIGHT_GRAY: "#DBD7D2",
+  WHITE: "#FFFFFF",
+};
