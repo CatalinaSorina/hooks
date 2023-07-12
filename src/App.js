b5afa9b3e394c030cfd70a9894a74c97ebbcf685
Catalina Sorina Gherzan <catalina.sorina@catalina.local>
2023-07-13 00:50:49 +0300

    updated App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback } from "react";
+import { Typography, styled } from "@mui/material";
+import usePhotos from "./hooks/usePhotos";
+import FriendsStatus from "./components/FriendStatus/FriendsStatus";
+import Photos from "./components/Photos/Photos";
+import Cart from "./components/Cart/Cart";
+
+const RootContainer = styled("div")({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "space-between",
+  height: "calc(100vh - 40px)",
+  padding: "20px",
+  textAlign: "center",
+  cursor: "default",
+});
 
 function App() {
+  const [cartItems, setCartItems] = useState([]);
+  const { photos, loading: loadingPhotos, error: errorPhotos, updatePhoto, updatePhotos } = usePhotos();
+
+  const handleAddToCart = useCallback(
+    (photo) => {
+      setCartItems((prevCartItems) => [...prevCartItems, photo]);
+      updatePhoto({ ...photo, isInCart: true });
+    },
+    [updatePhoto],
+  );
+
+  const handleRemoveFromCart = useCallback(
+    (photo) => {
+      setCartItems((prevCartItems) => prevCartItems.filter((item) => item.id !== photo.id));
+      updatePhoto({ ...photo, isInCart: false });
+    },
+    [updatePhoto],
+  );
+
+  const handleClearAll = useCallback(() => {
+    setCartItems([]);
+    updatePhotos({ isInCart: false });
+  }, [updatePhotos]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RootContainer>
+      <Typography variant="h4">Welcome!</Typography>
+      <Typography variant="h4">You can add photos to your cart:</Typography>
+      <Photos
+        photos={photos}
+        loading={loadingPhotos}
+        error={errorPhotos}
+        addToCart={handleAddToCart}
+        removeFromCart={handleRemoveFromCart}
+      />
+      <Cart cartItems={cartItems} removePhotoFromCart={handleRemoveFromCart} removeAll={handleClearAll} />
+      <FriendsStatus />
+    </RootContainer>
   );
 }
 
